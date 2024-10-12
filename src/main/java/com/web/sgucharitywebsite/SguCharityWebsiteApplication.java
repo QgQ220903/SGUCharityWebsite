e782008fb066750ee2c9613f4b82e359dc1b4247
QgQ220903 <quyquach22092003@gmail.com>
2024-10-12 16:35:22 +0700

    firts commit

---
+++ src/main/java/com/web/sgucharitywebsite/SguCharityWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.web.sgucharitywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguCharityWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SguCharityWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/sgucharitywebsite/SguCharityWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.sgucharitywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SguCharityWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
