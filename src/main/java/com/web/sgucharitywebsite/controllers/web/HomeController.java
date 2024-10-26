24438c2ed80d5f55967f876e8ed7615413eb58bd
QgQ220903 <quyquach22092003@gmail.com>
2024-10-26 15:27:35 +0700

    pushing all template

---
+++ src/main/java/com/web/sgucharitywebsite/controllers/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.web.sgucharitywebsite.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @RequestMapping("")
+    public String home() {
+        return "user/home/index";
+    }
+}
---
+++ src/main/java/com/web/sgucharitywebsite/entity/User.java
@@ -0,0 +1,78 @@
+package com.web.sgucharitywebsite.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+  private String username;
+  private String password;
+  private String fullName;
+  private LocalDate dateOfBirth;
+  private String email;
+  private String phone;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+}
