7cfc90628d70e7043032166ecf8194248549b9e2
Haquangnguyen30 <haquangnguyen30@gmail.com>
2024-11-15 21:53:46 +0700

    CRUD charity project

--- src/main/java/com/web/sgucharitywebsite/dto/ProjectDto.java
+++ src/main/java/com/web/sgucharitywebsite/dto/ProjectDto.java
@@ -1,6 +1,7 @@
 package com.web.sgucharitywebsite.dto;
 
 import com.web.sgucharitywebsite.entity.Category;
+import com.web.sgucharitywebsite.helper.CurrencyFormatter;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,8 +9,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 @Data
 @Builder
 @NoArgsConstructor
@@ -18,11 +20,31 @@ public class ProjectDto {
     private Long id;
     private String name;
     private String content;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private LocalDate startTime;
+
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private LocalDate endTime;
     private double targetAmount;
     private double currentAmount;
     private String status;
     private LocalDateTime createOn;
     private LocalDateTime updateOn;
+    private Long categoryId;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getFormattedTargetAmount() {
+        return CurrencyFormatter.formatToVND(targetAmount);
+    }
+
+    // Getter cho currentAmount đã định dạng
+    public String getFormattedCurrentAmount() {
+        return CurrencyFormatter.formatToVND(currentAmount);
+    }
 }
